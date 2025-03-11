a30a0b4173969433e917cda88db52082c3e48482
Noitidart <noitidart@protonmail.com>
2025-03-11 16:02:26 -0700

    Add test download image links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import { Inter } from '@next/font/google';
-import Head from 'next/head';
-import Image from 'next/image';
+import { Inter } from "@next/font/google";
+import Head from "next/head";
+import Image from "next/image";
 
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -27,7 +27,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -116,6 +116,20 @@ export default function Home() {
               Instantly deploy your Next.js site to a shareable URL
               with&nbsp;Vercel.
             </p>
+
+            <a
+              href="https://whoa-alpha.vercel.app/thirteen.svg"
+              download="same-domain-thirteen.svg"
+            >
+              thirteen.svg
+            </a>
+
+            <a
+              href="https://whoa-alpha.vercel.app/next.svg"
+              download="same-domain-next.svg"
+            >
+              Next.svg
+            </a>
           </a>
         </div>
       </main>
