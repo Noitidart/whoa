8ecc8aec49f886227f0467fd0c6e47c7de288b9f
Noitidart <noitidart@protonmail.com>
2025-03-11 16:14:21 -0700

    Try without access control origin header

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,8 @@ export function middleware(request: NextRequest) {
   if (url.endsWith("/thirteen.svg")) {
     const response = NextResponse.next();
     // response.headers.set("Access-Control-Allow-Origin", "*");
+    // delete access-control-allow-origin
+    response.headers.delete("Access-Control-Allow-Origin");
     // response.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
     // response.headers.set(
     //   "Access-Control-Allow-Headers",
