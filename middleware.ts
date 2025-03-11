c311fdcb9805af42baddfb595924fa96a0ae2d21
Noitidart <noitidart@protonmail.com>
2025-03-11 15:57:16 -0700

    Add CORS methods

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,7 @@ export function middleware(request: NextRequest) {
   if (url.endsWith("/thirteen.svg")) {
     const response = NextResponse.next();
     response.headers.set("Access-Control-Allow-Origin", "*");
+    response.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
     return response;
   }
 
